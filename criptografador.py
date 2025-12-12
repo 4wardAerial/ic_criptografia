457ae0a597c0ffa1d0c436b25ba49c844ca8de2d
4wardAerial <4lucas.h.bertanha8@gmail.com>
2025-12-12 17:46:42 -0300

    Adiciona mais funções para o RSA

--- criptografador.py
+++ criptografador.py
@@ -1,6 +1,7 @@
+import random
 import conversor_msg as cm
-from erros import CharInvalidoError
-
+from pathlib import Path
+from funcoes import alg_euclides_extendido, escolha_e
 
 def RSA_cripto(partes_decimal: list[int], cpub_n: int, cpub_e: int) -> list[int]:
     partes_cripto = []
@@ -10,18 +11,36 @@ def RSA_cripto(partes_decimal: list[int], cpub_n: int, cpub_e: int) -> list[int]
 
 def RSA_decripto(partes_cripto: list[int], cpriv_d: int, cpub_n: int) -> list[int]:
     partes_decripto = []
-    for cyp in partes_cripto:
-        partes_decripto.append(pow(cyp, cpriv_d, cpub_n))
+    for cyph in partes_cripto:
+        partes_decripto.append(pow(cyph, cpriv_d, cpub_n))
     return partes_decripto
 
+def calculadora_chaves_RSA(p: int, q: int):
+    n: int = p * q
+    phi: int = (p - 1) * (q - 1)
+    e: int = escolha_e(phi)
+    mdc: tuple = alg_euclides_extendido(e, phi)
+    d: int = mdc[1] % phi
+    return n, e, d
+
 if __name__ == "__main__":
-    while(True):
-        msg = input("Qual mensagem: ")
-        
-        try:
-            lista_dec = cm.converter_para_decimal(msg, 10)
-            
-            print("Obrigado por usar nossos sistemas\n")
-            break
-        except CharInvalidoError as err:
-            print(f"Caractere inválido: {err.char}. Tente novamente.\n")
+    DIR = Path(__file__).resolve().parent
+    primos_txt = DIR / "arquivos" / "primos.txt"
+    with primos_txt.open(encoding="utf-8") as ptxt:
+        primos = ptxt.readlines()
+        total_primos = len(primos)
+        p = int(primos[random.randrange(0, total_primos, 2)])
+        q = int(primos[random.randrange(1, total_primos, 2)])
+
+    n, e, d = calculadora_chaves_RSA(p, q)
+
+    msg_teste = "Msg bem grande essa kkkkkkk ou sera?"
+    max_tam = len(str(n)) - 1
+    partes_decimal = cm.converter_para_decimal(msg_teste, max_tam)
+    print(partes_decimal)
+
+    partes_cripto = RSA_cripto(partes_decimal, n, e)
+    print(partes_cripto)
+
+    partes_decripto = RSA_decripto(partes_cripto, d, n)
+    print(partes_decripto)
---
+++ funcoes.py
@@ -0,0 +1,30 @@
+import random
+
+'''
+Calcula o MDC entre dois números pelo método de euclides das divisões
+sucessivas e os valores de m e n tais que MDC(a, b) = ma + nb.
+'''
+def alg_euclides_extendido(a: int, b: int) -> tuple:
+    m1, n1 = 1, 0
+    m2, n2 = 0, 1
+
+    while b:
+        q = a // b
+        r = a % b
+
+        a = b
+        b = r
+        m1, m2 = m2, m1 - q * m2
+        n1, n2 = n2, n1 - q * n2
+
+    return (a, m1, n1)
+
+'''
+Gera um valor para e, 1 < e < phi(n), coprimo com o totiente (phi(n)) para
+ser usado no RSA.
+'''
+def escolha_e(phi: int) -> int:
+    while True:
+        e = random.randrange(3, phi - 1, 2)
+        if alg_euclides_extendido(e, phi)[0] == 1:
+            return e
