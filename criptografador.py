2069e1ce73855209a04b18770c482b8bfb57406d
4wardAerial <4lucas.h.bertanha8@gmail.com>
2025-12-12 16:09:52 -0300

    Remove interface e adicionar criptografia RSA

--- criptografador.py
+++ criptografador.py
@@ -1,13 +1,26 @@
 import conversor_msg as cm
 from erros import CharInvalidoError
 
+
+def RSA_cripto(partes_decimal: list[int], cpub_n: int, cpub_e: int) -> list[int]:
+    partes_cripto = []
+    for msg in partes_decimal:
+        partes_cripto.append(pow(msg, cpub_e, cpub_n))
+    return partes_cripto
+
+def RSA_decripto(partes_cripto: list[int], cpriv_d: int, cpub_n: int) -> list[int]:
+    partes_decripto = []
+    for cyp in partes_cripto:
+        partes_decripto.append(pow(cyp, cpriv_d, cpub_n))
+    return partes_decripto
+
 if __name__ == "__main__":
     while(True):
         msg = input("Qual mensagem: ")
         
         try:
             lista_dec = cm.converter_para_decimal(msg, 10)
-            print(lista_dec)
+            
             print("Obrigado por usar nossos sistemas\n")
             break
         except CharInvalidoError as err:
--- interface.py
+++ interface.py
@@ -12,38 +12,11 @@ def carregar_usuarios():
         return {linha["nome"]: linha for linha in reader}
 
 def cadastro(nome: str):
-    print("\n-------------------------------------------------")
-    print("Para gerarmos suas chaves, vamos precisar de números primos.")
-    print("\nDigite o número referente a sua escolha:")
-    print("1. Fornecer os próprios primos")
-    print("2. Usar primos aleatórios")
-    escolha_primos = int(input("> "))
-
-    if escolha_primos == 1:
-        print("Digite seu primeiro primo:")
-        p = int(input("> "))
-        print("Digite seu segundo primo:")
-        q = int(input("> "))
-    elif escolha_primos == 2:
-        pass
-    else:
-        print("Entrada inválida! Primos serão escolhidos aleatoriamente.")
+    pass
         
 
 def login():
-    usuarios = carregar_usuarios()
-
-    print("\n-------------------------------------------------")
-    print("Bem-vind@ ao sistema. Por favor, insira seu nome:")
-    nome = input("> ")
-
-    if nome not in usuarios:
-        print("\nNome não registrado no sistema. Vamos iniciar o cadastro.")
-        time.sleep(1)
-        cadastro(nome)
-    else:
-        print("Insira sua chave privada:")
-        chave_priv = input("> ")
+    pass
 
 login()
 
